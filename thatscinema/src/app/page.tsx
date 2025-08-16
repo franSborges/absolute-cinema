e98bb2ec56aeb60ed51bd86706863de86ffd9387
Fran Borges <borgesfran988@gmail.com>
2025-08-15 23:46:12 -0300

    feat(home) home in progress

--- thatscinema/src/app/page.tsx
+++ thatscinema/src/app/page.tsx
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-       <div className="title">
-        <h1>That´s Cinema</h1>
-        <p>Find where you can watch your movie.</p>
+       <div className={styles.containerTitles}>
+        <h1 className={styles.title}>That´s Cinema</h1>
+        <p className={styles.paragraph}>Find where you can watch your movie.</p>
        </div>
 
-       <div className="search-movie">
-        <span>search...</span>
+       <div className={styles.containerInput}>
+        <input className={styles.searchInput} type="search" name="search..." id="" />
        </div>
       </main>
     </div>
